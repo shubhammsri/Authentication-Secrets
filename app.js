a4c8e1166fcfe3260cc75835a5b834f318c0363e
Shubham Srivastava <shubhammsri@gmail.com>
2022-11-02 17:52:12 +0530

    Level 3

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"))
 app.set('view engine','ejs')
@@ -22,9 +20,6 @@ const userSchema = new mongoose.Schema({
   email:String,
   password:String
 });
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User= new mongoose.model("User", userSchema);
 
@@ -44,7 +39,7 @@ app.post("/register",function(req, res) {
 
   const newUser = new User({
        email:req.body.username,
-       password:req.body.password
+       password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -57,7 +52,7 @@ app.post("/register",function(req, res) {
 
 app.post("/login",function(req, res){
   const username= req.body.username;
-  const password= req.body.password;
+  const password= md5(req.body.password);
 
    User.findOne({email:username},function(err,foundUser){
     if(err){
@@ -72,9 +67,6 @@ app.post("/login",function(req, res){
   });
 });
 
-
-
-
 app.listen(3000,function(req,res){
   console.log("Server started on port 3000.");
-})
+});
